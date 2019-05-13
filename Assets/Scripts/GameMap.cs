c3efcd27a75529c8238475ec53f0b18e5c7cfa80
Chris Lochhead <chrislochhead123@outlook.com>
2019-05-13 21:49:05 +0100

    worked out quaternion rotations for recording them into map files, building temporary map for gameplay implementation

--- Assets/Scripts/GameMap.cs
+++ Assets/Scripts/GameMap.cs
@@ -34,17 +34,17 @@ public class GameMap : MonoBehaviour {
             {
                 if (mapInfo[i].GetComponent<Wall>().type == 0)
                 {
-                    Instantiate(redWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 0), Quaternion.identity);
+                    Instantiate(redWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 3), Quaternion.identity * Quaternion.Euler(0,0, mapInfo[i].GetComponent<Wall>().rot));
                 }
                 else
                 if (mapInfo[i].GetComponent<Wall>().type == 1)
                 {
-                    Instantiate(orangeWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 0), Quaternion.identity);
+                    Instantiate(orangeWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 3), Quaternion.identity);
                 }
                 else
                 if (mapInfo[i].GetComponent<Wall>().type == 2)
                 {
-                    Instantiate(greenWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 0), Quaternion.identity);
+                    Instantiate(greenWall, new Vector3(mapInfo[i].GetComponent<Wall>().pos.x, mapInfo[i].GetComponent<Wall>().pos.y, 3), Quaternion.identity);
                 }
             }else
                 if (mapInfo[i].GetComponent<Coin>())
