d559293c81850667785f09a40af4fe2581d30d72
Chris Lochhead <chrislochhead123@outlook.com>
2019-05-15 19:02:32 +0100

    added player rotations, modified player object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,26 +15,30 @@ public class Player : MonoBehaviour {
 	void Update () {
 
         Ray cameraRay = playerCam.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        Plane ground = new Plane(Vector3.forward, Vector3.zero);
         float rayLength;
 
-        //if(ground.Raycast(cameraRay, out rayLength))
-        //{
-        //    transform.LookAt(new Vector3(cameraRay.GetPoint(rayLength).x, transform.position.y, -10));
-        //}
+        //get mouse position and apply transformation on z-coordinate to make it level with the player
         Vector3 mPos = Input.mousePosition;
         mPos.z = 190.0f;
 
+        //get its position in world space
         mPos = Camera.main.ScreenToWorldPoint(mPos);
+
+        if (ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 target = cameraRay.GetPoint(rayLength);
+            Vector3 direction = target - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            //transform.rotation = Quaternion.Euler(-90, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 0, -rotation);
+
+        }
 
         if (Input.GetKey("w"))
         {
-
-           // Vector3 movementTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);// playerCam.ScreenToWorldPoint(Input.mousePosition);
+            //apply the move toward function using this position
             transform.position = Vector3.MoveTowards(transform.position, mPos, 2);
-           // Vector3 pos = transform.position;
-            //pos.z = -10.0f;
-           // transform.position = pos;
         }
 
         
