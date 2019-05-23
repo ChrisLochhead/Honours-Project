55d17d43143dd4c8bd22902a2c4932a9aa329183
Chris Lochhead <chrislochhead123@outlook.com>
2019-05-23 17:10:50 +0100

    started setting up the build menu and the drop down menus

---
+++ Assets/Scripts/BuildMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BuildMenu : MonoBehaviour {
+
+    public GameObject openState;
+    public GameObject closedState;
+
+    int currentState;
+
+    public Sprite[] buttons;
+
+	// Use this for initialization
+	void Start () {
+        currentState = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CloseClicked()
+    {
+        if (currentState == 0)
+        {
+            openState.SetActive(true);
+            closedState.SetActive(false);
+            currentState = 1;
+        }
+        else
+        {
+            openState.SetActive(false);
+            closedState.SetActive(true);
+            currentState = 0;
+        }
+    }
+}
