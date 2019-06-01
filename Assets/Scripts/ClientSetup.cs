209bc365e3b521cb4b766501249ca229159a6d2f
Chris Lochhead <chrislochhead123@outlook.com>
2019-06-01 16:11:43 +0100

    made game dependent on prefabs, completed initial networking challenges.

---
+++ Assets/Scripts/ClientSetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClientSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] clientComponents;
+
+    Camera sceneCam;
+
+	// Use this for initialization
+	void Start () {
+
+        sceneCam = Camera.main;
+
+        if(!isLocalPlayer)
+        {
+            //Disable all components unique to the client
+            for(int i = 0; i < clientComponents.Length; i++)
+            {
+                clientComponents[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCam.gameObject.SetActive(false);
+        }
+	}
+
+    private void OnDisable()
+    {
+        if (sceneCam)
+            sceneCam.gameObject.SetActive(true);
+    }
+}
