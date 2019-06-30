a978df83ac5d0b402500245c7d38dcb93bbc5227
Chris Lochhead <chrislochhead123@outlook.com>
2019-06-30 18:31:23 +0100

    added more to lobby system, almost ready to begin preliminary testing

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using TMPro;
 
-public class Lobby : MonoBehaviour {
+public class Lobby : NetworkBehaviour {
 
     [SyncVar] public bool hasGameStarted = false;
 
     [SyncVar] public int NumberOfPlayers = 0;
 
+    private int currentNumberOfPlayers = 0;
+
+    public GameObject[] playerTags;
+
     private int MinNumOfPlayers = 2;
 
     public float timeTillGameStart = 30.0f;
+
+    public TextMeshProUGUI status;
 
     public void startGame()
     {
@@ -28,27 +35,73 @@ public class Lobby : MonoBehaviour {
     [ClientRpc]
     public void RpcCountDown()
     {
-
+        timeTillGameStart -= Time.deltaTime;
     }
 
     public void CountDown()
     {
-
+        timeTillGameStart -= Time.deltaTime;
     }
+
+    [Command]
+    public void CmdRefreshLobby()
+    {
+        RefreshLobby();
+        RpcRefreshLobby();
+    }
+
+    [ClientRpc]
+    public void RpcRefreshLobby()
+    {
+        currentNumberOfPlayers = 0;
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Client"))
+        {
+            playerTags[NumberOfPlayers].GetComponentInChildren<TextMeshProUGUI>().text = g.GetComponent<Client>().playerName;
+            NumberOfPlayers++;
+            currentNumberOfPlayers++;
+        }
+    }
+
+    public void RefreshLobby()
+    {
+        currentNumberOfPlayers = 0;
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Client"))
+        {
+            playerTags[NumberOfPlayers].GetComponentInChildren<TextMeshProUGUI>().text = g.GetComponent<Client>().playerName;
+            NumberOfPlayers++;
+            currentNumberOfPlayers++;
+        }
+    }
+
     private void Update()
     {
         NumberOfPlayers = 0;
-        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Client").Length; i++)
-        {
-            NumberOfPlayers++;
-        }
 
+        GameObject[] clients = GameObject.FindGameObjectsWithTag("Client");
+
+        if(clients.Length > currentNumberOfPlayers)
+        {
+            CmdRefreshLobby();
+        }
+        
         if(NumberOfPlayers >= MinNumOfPlayers)
         {
-            
-
+            CmdCountDown();
+        }
+        else
+        {
+            timeTillGameStart = 30.0f;
         }
 
+        if(timeTillGameStart <= 0)
+        {
+            //Start the game by removing the lobby
+            foreach(GameObject g in clients)
+            {
+                g.GetComponent<Client>().InitialisePlayer();
+            }
+        }
 
+        status.text = "Game will begin in" + ((int)timeTillGameStart).ToString() + "seconds";
     }
 }
